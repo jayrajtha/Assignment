65a60439d0447459bfeae71a0ef90767cf479acf
jayrajtha <jayrajtha13@gmail.com>
2022-11-14 17:48:59 +0530

    file added

---
+++ simpleIntrest.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c;  
+    printf("Enter your principle amount value: ");
+    scanf("%f",&a);
+    printf("Enter the rate of intrest: ");
+    scanf("%f",&b);
+    printf("Enter tenure in years: ");
+    scanf("%f",&c);
+    float x,y;
+    x=a*b*c*0.01;
+    printf("SIMPLE INTREST:%.2f\n",x);
+    y=x+a;
+    printf("AMOUNT:%.2f",y);
+    return 0;
+}
